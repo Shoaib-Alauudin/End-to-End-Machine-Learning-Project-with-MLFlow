7478d1b585e448a4a8de2a4f7f9c29b747bd2637
Shoaib Alauddin <shoaib.alaudin@gmail.com>
2023-08-06 23:27:19 +0500

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 End-to-End-Machine-Learning-Project-with-MLFlow
+
+
+## Workflow
+update config.yaml
+update schema.yaml
+update params.yaml
+update the entity
+update the configuration manager in src config
+update the components
+update the pipeline
+update the main.py
+update the app.py #dev.yaml
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from MLProject import logger
-
+from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
 #logger.info("Welcome to Custom log second time")
 
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} startedd <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=============X")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
