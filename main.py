9b34b1672658d363b615a6734e7528ed5b61da95
Shoaib Alauddin <shoaib.alaudin@gmail.com>
2023-08-14 01:32:47 +0500

    model evaluation pipeline completed and added MLFlow to the project

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from MLProject import logger
 from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-from MLProject.pipeline.stage_04_model_trainer import ModelTrainingTrainingPipeline
+from MLProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from MLProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 #logger.info("Welcome to Custom log second time")
 
@@ -41,7 +42,17 @@ except Exception as e:
 STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   model_trainer = ModelTrainingTrainingPipeline()
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelEvaluationTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
